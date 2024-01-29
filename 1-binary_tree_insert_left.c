3092e2a645526d57e5a836817402a315031b1789
Omar Ahmed O <deeyndiid@gmail.com>
2024-01-30 00:19:49 +0300

    Add task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,4 +10,30 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node;
+	binary_tree_t *grand_child;
+
+	if (parent == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->right = NULL;
+	node->left = NULL;
+
+	if (parent->left == NULL)
+		parent->left = node;
+	else
+	{
+		grand_child = parent->left;
+		parent->left = node;
+		node->left = grand_child;
+		grand_child->parent = node;
+	}
+
+	return (node);
 }
