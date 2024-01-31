069ec2ef26298649c262968d4b5353f32565eb1b
Omar Ahmed O <deeyndiid@gmail.com>
2024-01-31 05:01:12 +0300

    Update task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,10 +2,29 @@
 
 /**
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
- * 
+ *
  * @first: First node
  * @second: Second node
- * Returns: NULL if no common ancestor was found
+ * Return: NULL if no common ancestor was found
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
-{}
+binary_tree_t *binary_trees_ancestor(
+		const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *tmp;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	while (first)
+	{
+		tmp = (binary_tree_t *) second;
+		while (tmp)
+		{
+			if (tmp == first)
+				return ((binary_tree_t *) first);
+			tmp = tmp->parent;
+		}
+		first = first->parent;
+	}
+	return (NULL);
+}
