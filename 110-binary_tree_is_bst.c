c27092b59d420634e310a854d4648317c5fbb6d9
Victor <victormwendwa804@gmail.com>
2024-01-31 23:12:15 -0800

    Add task 24

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_bst - Checks if a binary tree is valid Binary Search Tree.
+ * @tree:
+ *
+ * Return
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+}
