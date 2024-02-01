1c446ea04a1b1434051e669cdf088496d66980c2
Victor <victormwendwa804@gmail.com>
2024-01-31 21:52:44 -0800

    Added 21

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder -
+ * @tree: Pointer to root node.
+ * @func: Pointer to a function to call for each node
+ *
+ * Return: Do nothing if tree or func is NULL.
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+{
+}
